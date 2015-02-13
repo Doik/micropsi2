d6732eea63d12661a6d3edc305989d3ff287a409
Dominik Welland <hallo@doik.de>
2015-02-13 17:40:55 +0100

    fix movment

--- micropsi_core/world/minecraft/spockplugin.py
+++ micropsi_core/world/minecraft/spockplugin.py
@@ -1,6 +1,5 @@
 import logging
-from spock.mcmap import smpmap
-from spock.mcp import mcdata, mcpacket
+from spock.mcp import mcdata
 from spock.mcp.mcpacket import Packet
 from spock.utils import pl_announce
 from spock.mcmap import mapdata
@@ -8,7 +7,7 @@ import math
 
 
 STANCE_ADDITION = 1.620
-STEP_LENGTH = 1.0
+STEP_LENGTH = 0.5
 JUMPING_MAGIC_NUMBER = 0  # 2 used to work
 
 
@@ -44,34 +43,93 @@ class MicropsiPlugin(object):
         return self.net.connected and self.net.proto_state
 
     def dispatchMovement(self, move_x, move_z):
-        target_coords = self.get_int_coordinates()
-        target_coords['x'] += int(move_x)
-        target_coords['z'] += int(move_z)
+        target_coords = self.get_coordinates()
 
-        ground_offset = 2  # assume impossible
-        y = target_coords['y'] - 1  # current block agent is standing on
+        # move +/- STEP_LENGTH, depending on parameter signedness
+        if move_x:
+            target_coords['x'] += math.copysign(STEP_LENGTH, move_x)
+        if move_z:
+            target_coords['z'] += math.copysign(STEP_LENGTH, move_z)
 
-        # check if the next step is possible: nothing in the way, height diff <= 1
-        if self.is_opaque(self.get_block_type(target_coords['x'], y + 2, target_coords['z'])):
-            ground_offset = 2
-        elif self.is_opaque(self.get_block_type(target_coords['x'], y + 1, target_coords['z'])) and \
-                not self.is_opaque(self.get_block_type(target_coords['x'], y + 3, target_coords['z'])):
-            ground_offset = 1
-        elif self.is_opaque(self.get_block_type(target_coords['x'], y, target_coords['z'])):
-            ground_offset = 0
-        elif self.is_opaque(self.get_block_type(target_coords['x'], y - 1, target_coords['z'])):
-            ground_offset = -1
+        # avoid standing on block edges for easier calculation
+        if target_coords['x'] % 0.5 == 0:
+            target_coords['x'] -= 0.1
+        if target_coords['z'] % 0.5 == 0:
+            target_coords['z'] -= 0.1
 
-        # TODO: Currently we would walk through lava and water.
+        # collect target blocks (1): block at coordinates
+        block_coords = [self.get_block_coordinates(target_coords)]
 
-        if ground_offset < 2:
-            self.clientinfo.position['x'] = target_coords['x'] + .5
-            self.clientinfo.position['y'] = target_coords['y'] + ground_offset
-            self.clientinfo.position['stance'] = target_coords['y'] + ground_offset + STANCE_ADDITION
-            self.clientinfo.position['z'] = target_coords['z'] + .5
-            self.clientinfo.position['on_ground'] = True
-            return True
-        return False
+        # collect target blocks (2): blocks we can are too close to:
+        # since Steve is a fatty, movement is influenced by surrounding blocks if distance is smaller than 0.3
+        dist = {
+            'x': round((abs(target_coords['x']) % 1), 4),
+            'z': round((abs(target_coords['z']) % 1), 4)
+        }
+        if not 0.3 <= dist['x'] <= 0.7 or not 0.3 <= dist['z'] <= 0.7:
+            # we are too close to the next block, need to check that one too:
+            for direction in ['x', 'z']:
+                if dist[direction] < 0.3:
+                    block_coords.append(self.get_block_coordinates(target_coords))
+                    block_coords[-1][direction] -= int(math.copysign(1, target_coords[direction]))
+                elif dist[direction] > 0.7:
+                    block_coords.append(self.get_block_coordinates(target_coords))
+                    block_coords[-1][direction] += int(math.copysign(1, target_coords[direction]))
+
+        # collect ground offsets for every block we need to check
+        ground_offset = []
+        y = target_coords['y'] - 1  # this is the current block the agent is standing on
+
+        # check movement possibility for every block
+        for check_block in block_coords:
+            move = False
+            # print('checking block %d / %d / %d' % (check_block['x'], check_block['y'], check_block['z']))
+            # check if the next step is possible: nothing in the way, height diff <= 1
+            if self.is_opaque(self.get_block_type(check_block['x'], y + 2, check_block['z'])):
+                move = False
+            elif self.is_opaque(self.get_block_type(check_block['x'], y + 1, check_block['z'])) and \
+                    not self.is_opaque(self.get_block_type(check_block['x'], y + 3, check_block['z'])):
+                # check if we got a special block with height > 1 that we can not jump onto
+                # (Door, IronDoor, Fence, Fence Gate, Cobblestone Wall)
+                if self.get_block_type(check_block['x'], y + 1, check_block['z']) not in [64, 71, 85, 107, 139]:
+                    ground_offset.append(1)
+                    move = True
+            elif self.is_opaque(self.get_block_type(check_block['x'], y, check_block['z'])):
+                ground_offset.append(0)
+                move = True
+            elif self.is_opaque(self.get_block_type(check_block['x'], y - 1, check_block['z'])):
+                ground_offset.append(-1)
+                move = True
+            if not move:
+                print('nope')
+                return False
+
+        # take the highest block
+        # (necessary, if we're close to other blocks)
+        ground_offset.sort()
+        ground_offset = ground_offset[-1]
+
+        # TODO: what block do we use as standing on here?
+        block_coords = block_coords[-1]
+
+        # check for water:
+        foot_block = self.get_block_type(block_coords['x'], y + 1, block_coords['z'])
+        ground_block = self.get_block_type(block_coords['x'], y, block_coords['z'])
+        if ground_block in [8, 9]:
+            if foot_block in [8, 9]:
+                # we're already swimming
+                ground_offset = 0
+            else:
+                # we would be walking on water, adjust y to be *in* water
+                ground_offset = -1
+
+        # update our position
+        self.clientinfo.position['x'] = target_coords['x']
+        self.clientinfo.position['y'] = target_coords['y'] + ground_offset
+        self.clientinfo.position['stance'] = target_coords['y'] + ground_offset + STANCE_ADDITION
+        self.clientinfo.position['z'] = target_coords['z']
+        self.clientinfo.position['on_ground'] = True
+        return True
 
     def is_opaque(self, block_id):
         if block_id <= 0:
@@ -103,7 +161,7 @@ class MicropsiPlugin(object):
 
     def get_biome_info(self, pos=None):
         if pos is None:
-            pos = self.get_int_coordinates()
+            pos = self.get_block_coordinates()
         key = (pos['x'] // 16, pos['z'] // 16)
         columns = self.world.columns
         if key not in columns:
@@ -117,7 +175,7 @@ class MicropsiPlugin(object):
 
     def get_temperature(self, pos=None):
         if pos is None:
-            pos = self.get_int_coordinates()
+            pos = self.get_block_coordinates()
         biome = self.get_biome_info(pos=pos)
         if biome:
             temp = biome['temperature']
@@ -131,7 +189,7 @@ class MicropsiPlugin(object):
         """ Attempts to eat the held item. Assumes held item implements eatable """
         logging.getLogger('world').debug('eating a bread')
         data = {
-            'location': self.get_int_coordinates(),
+            'location': self.get_coordinates(cast_int=True),
             'direction': -1,
             'held_item': {
                 'id': 297,
@@ -169,9 +227,20 @@ class MicropsiPlugin(object):
         # writes new data to clientinfo which is pulled and pushed to Minecraft by ClientInfoPlugin
         self.clientinfo.position = position
 
-    def get_int_coordinates(self):
+    def get_coordinates(self, cast_int=False):
+        x = int(self.clientinfo.position['x']) if cast_int else self.clientinfo.position['x']
+        z = int(self.clientinfo.position['z']) if cast_int else self.clientinfo.position['z']
         return {
-            'x': math.floor(self.clientinfo.position['x']),
-            'y': math.floor(self.clientinfo.position['y']),
-            'z': math.floor(self.clientinfo.position['z'])
+            'x': x,
+            'y': int(self.clientinfo.position['y']),
+            'z': z
+        }
+
+    def get_block_coordinates(self, pos=None):
+        if pos is None:
+            pos = self.get_coordinates()
+        return {
+            'x': math.floor(pos['x']),
+            'y': math.floor(pos['y']),
+            'z': math.floor(pos['z'])
         }
